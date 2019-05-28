fdf08a09972f63a960ab4f278bd89c305294d6a8
Trexnix <trexnix@gmail.com>
2019-05-28 11:51:28 +0700

    Upgrade HTTPoison deps version

--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule EliXero.Mixfile do
 
   defp deps do
     [
-      {:httpoison, "~> 0.9"},
+      {:httpoison, "~> 1.3"},
       {:ex_doc, ">= 0.0.0", only: :dev},
       {:poison, "~> 3.0"},
       {:ecto, "~> 3.1.1"}
